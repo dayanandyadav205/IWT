38a8d56d274693fe26e9a32dc8582177fe28b621
Dayanand Yadav <dayanandyadav205@gmail.com>
2026-09-24 20:24:56 +0530

    index.php to .html

--- footer.php
+++ footer.php
@@ -4,24 +4,10 @@
     <title></title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <style>
-       footer {
-            background-color: #1a1a24;
-            color: #ccc;
-            text-align: center;
-            margin-top: auto;
-            font-size: 0.9rem;
-        }
-
-    </style>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
-<br>
-<br>
-<br>
-<br>
-<br>
+
 <footer> &copy; 2023-2026 Designed & Developed by <a href="/contact.php" class="footer-link">Dayanand Yadav</a>, 
 Assistant Professor, Computer Science & Engineering Department, CDGI, Indore
 </footer>
--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Responsive Collapse Navigation Header</title>
-<link rel="stylesheet" href="style.css">
+<!-- <link rel="stylesheet" href="style.css"> -->
 
     </style>
 </head>
@@ -134,7 +134,7 @@
 
       <!-- Right Aligned Items -->
       <ul class="nav-menu nav-right">
-        <li class="nav-item"><a href="/form.php">Sign Up</a></li>
+        <li class="nav-item"><a href="/form.php">Register</a></li>
         <li class="nav-item"><a href="/login_user.php">User Login</a></li>
         <li class="nav-item"><a href="/login_admin.php">Admin Login</a></li>
         <li class="nav-item"><a href="/contact.php">Contact Us</a></li>
